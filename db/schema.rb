d0b2fcc0a464f83126458f07fb1d6c91aa4aa822
Freddy Henderson <hendersonsarah12@gmail.com>
2022-11-12 14:27:14 +0100

    added review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_132349) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_12_132636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_132349) do
     t.string "feature"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "venue_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+    t.index ["venue_id"], name: "index_reviews_on_venue_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,5 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_132349) do
     t.index ["venue_category_id"], name: "index_venues_on_venue_category_id"
   end
 
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "venues"
   add_foreign_key "venues", "venue_categories"
 end
