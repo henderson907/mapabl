c1327f1e939ef4fb19eada2282c7f00206ef1f41
Freddy Henderson <hendersonsarah12@gmail.com>
2022-11-15 19:56:27 +0100

    have removed the email column from the venue table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_134121) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_185542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,7 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_134121) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.string "email"
     t.string "name"
     t.bigint "venue_category_id", null: false
     t.datetime "created_at", null: false
