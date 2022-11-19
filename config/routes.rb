9b3260366ec4993bf7f3c365b1249b5be891b8da
Freddy Henderson <106918645+henderson907@users.noreply.github.com>
2022-11-19 11:52:52 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  get "about", to: "pages#about"
-  get "my_profile", to: "pages#my_profile"
-
   resources :venues do
     resources :reviews
   end
