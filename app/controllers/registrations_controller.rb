ad483e9f9ea3fb62977889701e9870fa4eea4d70
Pablo Munoz <pablo.munoz.sommaruga@gmail.com>
2022-11-26 12:56:47 +0100

    Edit page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
 
   private
-
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
     end
