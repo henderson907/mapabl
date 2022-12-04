598a2adf71841b2b67beeaa03f4b0ec27a7aa750
Pablo Munoz <pablo.munoz.sommaruga@gmail.com>
2022-12-04 19:33:43 +0100

    Star rating implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_26_151418) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_03_102351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_26_151418) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reviews_on_user_id"
     t.index ["venue_id"], name: "index_reviews_on_venue_id"
+  end
+
+  create_table "saved_venues", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "venue_id", null: false
+    t.index ["user_id"], name: "index_saved_venues_on_user_id"
+    t.index ["venue_id"], name: "index_saved_venues_on_venue_id"
   end
 
   create_table "users", force: :cascade do |t|
