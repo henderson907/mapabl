3ab61544dc6227e8db7d6ecce8b9c1aaab1883e6
Freddy Henderson <hendersonsarah12@gmail.com>
2023-06-03 12:21:37 +0100

    added saved venues join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_03_111443) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_03_112057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_111443) do
     t.index ["venue_id"], name: "index_reviews_on_venue_id"
   end
 
+  create_table "saved_venues", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "venue_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_saved_venues_on_user_id"
+    t.index ["venue_id"], name: "index_saved_venues_on_venue_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -107,6 +116,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_111443) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "venues"
+  add_foreign_key "saved_venues", "users"
+  add_foreign_key "saved_venues", "venues"
   add_foreign_key "venue_features", "accessibility_features"
   add_foreign_key "venue_features", "venues"
   add_foreign_key "venues", "venue_categories"
