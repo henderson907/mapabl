120d20247600f6dcc942f1477b92f50162b40b93
Freddy Henderson <hendersonsarah12@gmail.com>
2023-06-23 19:03:02 +0100

    connected button to stimulus

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,12 +3,17 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
   static targets = ["togglableElement"]
+  static values = {
+    filter: Number
+  }
+
   connect() {
     console.log("Hello from toggle_controller.js")
   }
 
   fire() {
-    this.togglableElementTarget.classList.toggle("active");
-    console.log("button clicked")
+    // this.togglableElementTarget.classList.toggle("active");
+
+    console.log("button clicked - " + this.filterValue)
   }
 }
